4bef622e0072409129218c6fe855b78a3648fedb
Denisgyngazov <Denisgyngazov@gmail.com>
2018-09-26 10:17:50 +0700

    add label

---
+++ newNoteApp/UI/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  newNoteApp
+//
+//  Created by Денис Гынгазов on 26/09/2018.
+//  Copyright © 2018 Муленок Дмитрий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
