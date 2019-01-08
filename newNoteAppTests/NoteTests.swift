18f7d663572abc305946e37cda4de80ef5323d9a
Дмитрий <mulenokd@gmail.com>
2019-01-09 01:13:43 +0700

    Тесты для класса Note

--- newNoteAppTests/NoteTests.swift
+++ newNoteAppTests/NoteTests.swift
@@ -13,10 +13,11 @@ class NoteTests: XCTestCase {
     
     func testNameSetСorrectValue() {
         
+        let setup = "Смирнов"
         let note = Note(name: "", dateCreated: "String", dateModified: "String", detailText: "Текст заметки", category: NoteCategory.home)
-        try? note.setName(newValue: "Смирнов")
-        let expected = "Смирнов"
-        XCTAssertEqual(note.name, expected)
+        
+        XCTAssertNoThrow(try note.setName(newValue: setup))
+        
     }
     
     func testNameSetIncorrectValue(){
@@ -24,17 +25,34 @@ class NoteTests: XCTestCase {
         let setup = "Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов"
         let note = Note(name: "", dateCreated: "String", dateModified: "String", detailText: "Текст заметки", category: NoteCategory.home)
         
-        XCTAssertThrowsError(try note.setName(newValue: setup)) { error in
-            XCTAssertEqual(error as? NameErrorList, NameErrorList.outOfRange)
-        }
+        XCTAssertThrowsError(try note.setName(newValue: setup))
         
     }
     
-    func testGetNameValue(){
+    func testNameGetCorrectValue(){
+
+        let setup = "Смирнов"
+        let note = Note(name: "", dateCreated: "String", dateModified: "String", detailText: "Текст заметки", category: NoteCategory.home)
+        let expected = "Смирнов"
+        try? note.setName(newValue: setup)
         
+        XCTAssertEqual(note.name, expected)
     }
     
     func testNoteConstructor(){
+        
+        let note = Note(name: "Смирнов", dateCreated: "1 янв 2019", dateModified: "1 янв 2019", detailText: "Текст заметки", category: NoteCategory.home)
+        let expectedName = "Смирнов"
+        let expectedDateCreated = "1 янв 2019"
+        let expectedDateModified = "1 янв 2019"
+        let expectedDetailText = "Текст заметки"
+        let expectedCategory = NoteCategory.home
+        
+        XCTAssertEqual(note.name, expectedName)
+        XCTAssertEqual(note.dateCreated, expectedDateCreated)
+        XCTAssertEqual(note.dateModified, expectedDateModified)
+        XCTAssertEqual(note.detailText, expectedDetailText)
+        XCTAssertEqual(note.category, expectedCategory)
         
     }
     
